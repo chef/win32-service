4d22ca5e2cdee5aa2f52711afd7bf0f590826c19
Daniel Berger <djberg96@gmail.com>
2011-08-22 14:55:28 -0600

    Use block form for a File.open call, and change 'statue' to 'status' for some methods.

--- lib/win32/daemon0.rb
+++ lib/win32/daemon0.rb
@@ -17,7 +17,7 @@ include Windows::MSVCRT::String
 
 
 # Wraps SetServiceStatus.
-def set_the_service_statue(curr_state, exit_code, check_point, wait_hint)
+def set_the_service_status(curr_state, exit_code, check_point, wait_hint)
   service_status = 0.chr * 28 # sizeof(SERVICE_STATUS)
   
   # Disable control requests until the service is started.
@@ -92,16 +92,16 @@ begin
     end
     
     # Set the status of the service.
-    set_the_service_statue(state, NO_ERROR, 0, 0)
+    set_the_service_status(state, NO_ERROR, 0, 0)
 
     # Tell service_main thread to stop.
     if [SERVICE_CONTROL_STOP].include?(ctrl_code)
       wait_result = WaitForSingleObject(@end_event, INFINITE)
 
       if wait_result != WAIT_OBJECT_0
-        set_the_service_statue(SERVICE_STOPPED,GetLastError(),0,0)
+        set_the_service_status(SERVICE_STOPPED,GetLastError(),0,0)
       else
-        set_the_service_statue(SERVICE_STOPPED,NO_ERROR,0,0)
+        set_the_service_status(SERVICE_STOPPED,NO_ERROR,0,0)
       end
 
       SetEvent(@stop_event) 
@@ -123,9 +123,10 @@ begin
 
     service_name = argv[0]
     
-    f = File.new(ENV['TEMP']+"\\daemon#{ARGV[0]}","w")
-    f.print(argv.join(';'))
-    f.close
+    File.open(ENV['TEMP'] + "\\daemon#{ARGV[0]}", "w"){ |fh|
+      fh.print(argv.join(';'))
+    }
+
     SetEvent(@arg_event)
   
     # Register the service ctrl handler.
@@ -137,8 +138,8 @@ begin
     # but exit at this point.
     return false if @status_handle == 0
       
-    #The service has started.
-    set_the_service_statue(SERVICE_RUNNING, NO_ERROR, 0, 0)
+    # The service has started.
+    set_the_service_status(SERVICE_RUNNING, NO_ERROR, 0, 0)
     
     true
   end
