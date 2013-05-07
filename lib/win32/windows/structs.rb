414e20678aa3b5694a34c4782a46557ddc1b3154
Daniel Berger <djberg96@gmail.com>
2013-05-07 13:12:35 -0600

    Added constants and a struct.

--- lib/win32/windows/structs.rb
+++ lib/win32/windows/structs.rb
@@ -3,5 +3,17 @@ require 'ffi'
 module Windows
   module Structs
     extend FFI::Library
+
+    class SERVICE_STATUS < FFI::Struct
+      layout(
+        :dwServiceType, :ulong,
+        :dwCurrentState, :ulong,
+        :dwControlsAccepted, :ulong,
+        :dwWin32ExitCode, :ulong,
+        :dwServiceSpecificExitCode, :ulong,
+        :dwCheckPoint, :ulong,
+        :dwWaitHint, :ulong
+      )
+    end
   end
 end
