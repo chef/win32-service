1ad0ee67b405fc4c57a750231519cdd644ff840e
Daniel Berger <djberg96@gmail.com>
2013-06-16 09:43:20 -0600

    Some function declaration fixes.

--- lib/win32/windows/functions.rb
+++ lib/win32/windows/functions.rb
@@ -9,24 +9,24 @@ module Windows
     typedef :pointer, :ptr
     typedef :string, :str
 
+    ffi_convention :stdcall
+
     ffi_lib :kernel32
 
     attach_function :CloseHandle, [:handle], :bool
-    attach_function :CreateEvent, [:ptr, :bool, :bool, :str], :handle
+    attach_function :CreateEvent, :CreateEventA, [:ptr, :bool, :bool, :str], :handle
     attach_function :CreateThread, [:ptr, :size_t, :ptr, :ptr, :dword, :ptr], :handle, :blocking => true
     attach_function :EnterCriticalSection, [:ptr], :void
     attach_function :FormatMessage, :FormatMessageA, [:ulong, :ptr, :ulong, :ulong, :str, :ulong, :ptr], :ulong
     attach_function :InitializeCriticalSection, [:ptr], :void
     attach_function :LeaveCriticalSection, [:ptr], :void
-    attach_function :RegisterServiceCtrlHandlerEx, [:str, :handler_ex, :ptr], :handle
     attach_function :SetEvent, [:handle], :bool
-    attach_function :SetServiceStatus, [:handle, :ptr], :bool
     attach_function :WaitForSingleObject, [:handle, :dword], :dword
     attach_function :WaitForMultipleObjects, [:dword, :ptr, :bool, :dword], :dword
 
     ffi_lib :advapi32
 
-    callback :handler_ex, [:ulong, :ulong, :pt, :pt], :void
+    callback :handler_ex, [:ulong, :ulong, :ptr, :ptr], :void
 
     attach_function :CloseServiceHandle, [:handle], :bool
 
@@ -55,6 +55,8 @@ module Windows
     attach_function :QueryServiceConfig, :QueryServiceConfigA, [:handle, :ptr, :dword, :ptr], :bool
     attach_function :QueryServiceConfig2, :QueryServiceConfig2A, [:handle, :dword, :ptr, :dword, :ptr], :bool
     attach_function :QueryServiceStatusEx, [:handle, :int, :ptr, :dword, :ptr], :bool
+    attach_function :RegisterServiceCtrlHandlerEx, :RegisterServiceCtrlHandlerExA, [:str, :handler_ex, :ptr], :handle
+    attach_function :SetServiceStatus, [:handle, :ptr], :bool
     attach_function :StartService, :StartServiceA, [:handle, :dword, :ptr], :bool
     attach_function :StartServiceCtrlDispatcher, :StartServiceCtrlDispatcherA, [:ptr], :bool, :blocking => true
   end
