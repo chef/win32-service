1572468c6f6a93434f95486ed8e8f661796d1eb1
Daniel Berger <djberg96@gmail.com>
2015-07-14 21:39:58 -0500

    Version bump.

--- test/test_win32_service.rb
+++ test/test_win32_service.rb
@@ -52,7 +52,7 @@ class TC_Win32_Service < Test::Unit::TestCase
   end
 
   test "version number is expected value" do
-    assert_equal('0.8.6', Win32::Service::VERSION)
+    assert_equal('0.8.7', Win32::Service::VERSION)
   end
 
   test "services basic functionality" do
