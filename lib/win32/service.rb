f0ad36d900ad1d2a1b0715bea15f86258652b198
Daniel Berger <djberg96@gmail.com>
2015-07-14 21:39:46 -0500

    Version bump.

--- lib/win32/service.rb
+++ lib/win32/service.rb
@@ -17,7 +17,7 @@ module Win32
     extend Windows::Functions
 
     # The version of the win32-service library
-    VERSION = '0.8.6'
+    VERSION = '0.8.7'
 
     # SCM security and access rights
 
